cce0da5ba0a9ccee54b337b6b5b071b45fbd5dc2
diaz92092 <diaz92092@gmail.com>
2021-05-22 19:45:59 +0800

    Delete keys.js

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-// keys.js - figure out what set of keys to return
-
-if (process.env.NODE_ENV === "production") {
-  // we are in production - return the production set of keys
-  module.exports = require("./prod");
-} else {
-  // we are in development - return the development set of keys
-  module.exports = require("./dev");
-}
